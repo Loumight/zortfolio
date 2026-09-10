b11f7cb86fd50e8c49b364d180049540747b0e17
Loumight <jdbbilling@gmail.com>
2026-09-10 17:40:17 +1000

    lapotpupload

--- src/pages/musicabt.tsx
+++ src/pages/musicabt.tsx
@@ -1,6 +1,92 @@
-export default function MAboutContent () {
-    return(
-<div className="h-full w-full bg-amber-500" >Hi THIS WORKS</div>
-    )
+export default function MAboutContent() {
+  return (
+    <div className="animate-reveal w-full flex backdrop-blur-xs backdrop-opacity-90 border-[hsl(120,20%,5%)]/20 border-4 text-xl text-white shadow-xl flex-col p-12">
+      <div className="flex flex-col min-h-0 mb-20 justify-center items-baseline">
+        <h3 className="text-5xl font-bold flex justify-center w-full">
+          <span className="highlighter px-12 py-4">
+            Welcome to my music portfolio!
+          </span>
+        </h3>
+        <div className="font-light text-sm flex justify-center w-full">
+          <p>I wonder how many people visit the music portfolio first?</p>
+        </div>
+      </div>
+      <h3 className="mb-10 text-3xl font-bold underline decoration-pink-500">
+        Why do I make music?
+      </h3>
+      {/*What on earth are these {" "} things bro */}
+      <p className="mb-6">
+        Real quick, some of my answers will refernce or compare to my art
+        portfolio's answers so I DO recommend to read that first, but its not
+        neccesary. My answer to this question is very similar to why I make art.
+        Wanting to make something that makes people go
+        <i>'Damn I wish I could make that'</i>. I want to leave an impression of
+        awe - and a bit of jealousy (In a good way), like how I hold Louis Cole
+        or Yoshimasa Terui. Like I heard
+      </p>
+      <p className="mb-10 font-bold">
+        "A lot of times I’ll sit down at a piano and I’ll just sound really
+        stupid. It’s like a mysterious world to me every time. I think it’s
+        actually helped me as opposed to hurt me." - Louis Cole
+      </p>
+      <h3 className="mb-10 text-3xl font-bold underline decoration-pink-500">
+        Who or what inspires me?
+      </h3>
+      <p className="mb-6">
+        My Inspiration for music is very different to art. Those moments are
+        fewer than art, but the urge to create after hearing{" "}
+        <i>
+          <b>that song</b>
+        </i>{" "}
+        is insurmountable. I was exposed to the song <i>Antiparallelogram</i> by
+        Onefin recently and I just had to pause the song and start playing it on
+        the drums. I feel like with music you almost have to make an effot to
+        find that new sound. Some of those gems include:
+      </p>
+      <div className="mb-20 grid grid-cols-3 gap-6">
+        <div>
+          <p className="mb-6">
+            The entire Zelda franchise (once again) cause its the best series
+            EVER. OoT and BotW/TotK have some BANGERS, but I also love the music from Twighlight Princess and Skyward Sword.
+          </p>
+          <img
+            className="rounded-lg"
+            src="https://i.pinimg.com/736x/21/d0/9d/21d09d3cc3b354ea1a373a226ddeddb7.jpg"
+            alt="Zelda Art"
+          ></img>
+        </div>
 
+        <div>
+          <p className="mb-6">
+            My favourite artists like: Louis Cole, Yoshimasa Terui, Haisuinonasa, and DM Dokuro. Theres more but for the sake of brevity, ive only listed the best.
+          </p>
+          <img
+            className="rounded-lg"
+            src="https://i.pinimg.com/736x/21/d0/9d/21d09d3cc3b354ea1a373a226ddeddb7.jpg"
+            alt="Show Art"
+          ></img>
+        </div>
+        <div>
+          <p className="mb-6">
+            Games such as Ror2, Celeste, Pseudoregalia, Neon White, and
+            Deltarune. Same deal with the artists, theres too many to list.
+          </p>
+          <img
+            className="rounded-lg"
+            src="https://i.pinimg.com/736x/21/d0/9d/21d09d3cc3b354ea1a373a226ddeddb7.jpg"
+            alt="Game Art"
+          ></img>
+        </div>
+      </div>
+
+      <h3 className="mb-10 text-3xl font-bold underline decoration-pink-500">
+        My art timeline
+      </h3>
+      <p className="mb-6"></p>
+      <h3 className="mb-10 text-3xl font-bold underline decoration-pink-500">
+        What resources do I use?
+      </h3>
+      <p className="mb-6"></p>
+    </div>
+  );
 }
